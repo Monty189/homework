6b9c74a53b8e5bd2a914d7d4e100b63065fded7c
Monty189 <ao.kamaev89@gmail.com>
2024-11-05 11:06:23 +0000

    Добавил дз3

---
+++ dz3/dz3_4.py
@@ -0,0 +1,4 @@
+user_input_a=float(input("Введите длинну основание треугольника: "))
+user_input_b=float(input("Введите длинну высоты треугольника: "))
+s=round((user_input_a*user_input_b)/2, 2)
+print(f"{"S треугольника = "}{s}")
---
+++ dz3/dz3_5.py
@@ -0,0 +1,5 @@
+user_input1 = int(input("Введите первое число: ")) 
+user_input2 = int(input("Введите второе число: ")) 
+user_input3 = int(input("Введите третье число: "))
+user_input4 = int(input("Введите четвертое число: "))
+print(f"{user_input4}{user_input3}{user_input2}{user_input1}")
