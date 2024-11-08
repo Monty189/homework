476784bde01178ea3c92ccbaa5a42e567b7e66a0
Monty189 <ao.kamaev89@gmail.com>
2024-11-08 17:47:02 +0000

    Добваил 2 пункт 6 дз

---
+++ dz6/dz6_2.py
@@ -0,0 +1,5 @@
+user_input = int(input("Введите целое число: "))
+
+number = [user_input**0, user_input**1, user_input**2, user_input**3, user_input**4, user_input**5, user_input**6, user_input**7]
+
+print(number)
