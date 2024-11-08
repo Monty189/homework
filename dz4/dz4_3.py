854386aff643fdfeb1d59876862608a443066831
Monty189 <ao.kamaev89@gmail.com>
2024-11-08 16:53:47 +0000

    Добавил 4 дз

---
+++ dz4/dz4_3.py
@@ -0,0 +1,10 @@
+number = int(input("Введите число: "))
+
+if number < 0:
+    print("Number is negativge")
+
+elif number == 0:
+    print("Number is equal to zero")
+
+else:
+    print("Number is positive")
