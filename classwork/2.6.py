ce37e68e61de0e65d1974a1a24d729a80dec1500
Monty189 <ao.kamaev89@gmail.com>
2024-11-15 17:55:27 +0000

    Добавлено 7 задание

--- classwork/2.6.py
+++ classwork/2.6.py
@@ -1,5 +1,5 @@
-user_input = int(input("Сколько секунд прошло: "))
-hour = round(user_input / 3600, 2) # Секунд в часе 
-min = round(user_input / 360, 2)  # Минут в часе
-sec = user_input // 60 # Целых секунд в минуте
+n = int(input("Сколько секунд прошло: "))
+hour = round(n / 3600, 2) # Секунд в часе 
+min = (n - hour * 3600) // 60  # Минут в часе
+sec = (n - ((hour * 3600) + (min * 60)))  # Целых секунд в минуте
 print(f"{hour}\n{min}\n{sec}")
---
+++ classwork/2.7.py
@@ -0,0 +1,9 @@
+ta=543
+th=130
+ts=round(ta*th)/2
+
+ks= 130 * 2
+
+k = ts//ks
+
+print(k, "квадратов")
