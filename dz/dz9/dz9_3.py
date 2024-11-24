946f5866ecfe10c8062534004ce93c9671d17e6d
Monty189 <ao.kamaev89@gmail.com>
2024-11-24 15:11:44 +0000

    Изменение в 3 задание

--- dz/dz9/dz9_3.py
+++ dz/dz9/dz9_3.py
@@ -1,10 +1,20 @@
 count = 0
 
 for number in range (100,10000):
-    digits = str(number)
+    digits = str (number)
 
-    if (digits[0] != digits[1] != digits[2]) or 
-        count += 1
+    if len(str(digits)) == 3:
+        if (digits[0] != digits[1] != digits[2]):
+
+            count += 1
+            
+        print(digits)
+
+    elif len(str(digits)) == 4:
+        if (digits[0] != digits[1] != digits[2] != digits[3]):
+
+            count +=1
+        
         print(digits)
 
 print(count)
