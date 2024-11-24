2c2fe092b540da4d17edbf7ebee561dd6450242f
Monty189 <ao.kamaev89@gmail.com>
2024-11-24 22:29:38 +0000

    обновлено

--- dz/dz10/dz10_2.py
+++ dz/dz10/dz10_2.py
@@ -1,1 +1,4 @@
-.
+for one_multiplier in range(1, 11):
+    for two_multiplier in range(1, 11):
+        print(f"{one_multiplier}{"x"}{two_multiplier}{"="}{one_multiplier*two_multiplier}", end="\t")
+    print()
--- dz/dz10/dz10_3.py
+++ dz/dz10/dz10_3.py
@@ -1,1 +1,8 @@
-.
+num1 = int(input("Введите первое число диапозона: "))
+num2 = int(input("Введите второе число диапозона: "))
+
+
+for one_multiplier in range(num1, num2 + 1):
+    for two_multiplier in range(1, 11):
+        print(f"{one_multiplier}{"x"}{two_multiplier}{"="}{one_multiplier*two_multiplier}", end='\t')
+    print()
