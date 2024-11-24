598b66ea97c0f8ad872c52785db9746b5202d3b2
Monty189 <ao.kamaev89@gmail.com>
2024-11-24 21:19:06 +0000

    add dz9_3

--- dz/dz9/dz9_3.py
+++ dz/dz9/dz9_3.py
@@ -1,20 +1,24 @@
 count = 0
 
+
+
 for number in range (100,10000):
-    digits = str (number)
+    digits = str(number)
+    
 
     if len(str(digits)) == 3:
-        if (digits[0] != digits[1] != digits[2]):
-
+        if (digits[0] != digits[1] and digits[0] != digits[2] and digits[1] != digits[2]):
+            print(digits)
             count += 1
             
-        print(digits)
+        
 
     elif len(str(digits)) == 4:
-        if (digits[0] != digits[1] != digits[2] != digits[3]):
-
+        if (digits[0] != digits[1] and digits[0] != digits[2
+        ] and digits[0] != digits[3] and digits[1] != digits[2
+        ] and digits[1] != digits[3] and digits[2] != digits[3]):
+            print(digits)
             count +=1
-        
-        print(digits)
-
+            
+            
 print(count)
