2e6d1fd2e70ab925072c67cfde63a89677f452dd
Monty189 <ao.kamaev89@gmail.com>
2024-11-25 13:15:16 +0000

    add dz10_1

--- dz/dz10/dz10_1.py
+++ dz/dz10/dz10_1.py
@@ -3,11 +3,14 @@ num2 = int(input("Введите конец диапозона: "))
 
 number = []
 
-for i in range(num1, num2 + 1):
-    if num2 % i == 0:
-        number.append(i)
+for i in range(num1, num2+1):
+    count = 0
+    for n in range(1, i+1):
+        if i % n == 0:
+            count += 1
+    if count < 3:
+        number.append(i) if i != 1 else None
+            
         
-    else:
-        continue
 
-print(number)
+print(*number)
