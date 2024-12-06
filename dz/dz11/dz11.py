d1d08f424a63c1f87f6674a9806c9ab1e05cea30
Monty189 <ao.kamaev89@gmail.com>
2024-12-06 18:39:45 +0000

    -

--- dz/dz11/dz11.py
+++ dz/dz11/dz11.py
@@ -1,1 +1,47 @@
-.
+#user_input = int(input("Введите длинну стороны квадрата: "))
+#for i in range(user_input):
+#    for j in range(user_input):
+#        print(" * ", end=' ')
+#    print("\n")
+
+def draw_triangle(fill, base=10): # Буква Б
+    n = 1
+    for  i in range(base):
+        i = [fill] * n
+        print (' '.join(i))
+        n += 1
+
+draw_triangle(' * ')
+
+print("------------------------------------------------")
+
+def draw_triangle(fill, base=10): # Буква И
+    n = 10
+    for i in range(base):
+        i = [fill] * n
+        print (' '.join(i))
+        n -= 1
+
+draw_triangle(' * ')
+
+print("------------------------------------------------")
+
+n = 5 # Буква К
+for i in range(n):
+    for j in range(n):
+        if j >= n - 1 - i:  # Условие для закрашивания половины
+            print("*", end = " ")
+        else: 
+            print(" ", end = " ")
+    print() # Переход на новою строку после каждой строки
+
+print("------------------------------------------------")
+
+n = 5 # Буква К
+for i in range(n):
+    for j in range(n):
+        if j <= n - 1 - i:  # Условие для закрашивания половины
+            print("*", end = " ")
+        else: 
+            print(" ", end = " ")
+    print() # Переход на новою строку после каждой строки
