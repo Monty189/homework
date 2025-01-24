1a1f2c62203f6a166f4b6e5075d79426642f1c23
Monty189 <ao.kamaev89@gmail.com>
2025-01-24 15:32:53 +0000

    add classwork

---
+++ classwork_js/classworkJS.js
@@ -0,0 +1,34 @@
+number = prompt('Введите число от 1 до 9: ')
+
+switch (number) {
+    case '0':
+        alert('0 - ноль');
+        break;
+    case '1':
+        alert('1 - один');
+        break;
+    case '2':
+        alert('2 - два');
+        break;
+    case '3':
+        alert('3 - три');
+        break;
+    case '4':
+        alert('4 - четыре');
+        break;
+    case '5':
+        alert('5 - пять');
+        break;
+    case '6':
+        alert('6 - шесть');
+        break;
+    case '7':
+        alert('7 - семь');
+        break;
+    case '8':
+        alert('8 - восемь');
+        break;
+    case '9':
+        alert('9 - девять');
+        break;
+}
